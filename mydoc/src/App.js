03f7c9686b1a61da480a83cb320845b70458c638
조혜연 <hy_312@mncnext.co.kr>
2022-01-24 09:05:22 +0900

    모바일

--- mydoc/src/App.js
+++ mydoc/src/App.js
@@ -9,16 +9,22 @@ import Footer from './component/layout/Footer'
 import Intro from './component/pages/Intro'
 import MainProductList from './component/pages/MainProductList'
 import DashBoard from './component/pages/DashBoard'
-
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
             <div className="bodyContainer">
-                {/*<Intro></Intro>*/}
-                {/*<MainProductList></MainProductList>*/}
-                <DashBoard></DashBoard>
+                <Route path="/intro">
+                    <Intro></Intro>
+                </Route>
+                <Route path="/" exact>
+                    <DashBoard></DashBoard>
+                </Route>
+                <Route path="/mainProductList">
+                    <MainProductList></MainProductList>
+                </Route>
             </div>
             {/*<Footer></Footer>*/}
         </div>
